c65bb4598d26f46c18c361235ed558c4047b6117
Brian Ellis <brian@wireless-10-147-17-95.public.utexas.edu>
2019-02-01 14:17:04 -0600

    can gen sin waves

--- test.py
+++ test.py
@@ -1,39 +1,46 @@
-import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 
-p = pyaudio.PyAudio()
+freqF = 87.307
 
-volume = 0.5     # range [0.0, 1.0]
+
 fs = 44100       # sampling rate, Hz, must be integer
-duration = 1.0   # in seconds, may be float
-f = 440.0        # sine frequency, Hz, may be float
+totalDuration = 540 # in seconds
+totalRes = np.zeros(fs*totalDuration)
 
-# generate samples, note conversion to float32 array
-samples = ( np.sin(2*np.pi*np.arange(fs*duration)*f/fs) ).astype(np.float32)
-samples = samples * volume
+def genSinWave(dur,freq):
+	duration = float(dur)   # in seconds, may be float
+	f = float(freq)        # sine frequency, Hz, may be float
+	samples = ( np.sin(2*np.pi*np.arange(fs*duration)*f/fs) ).astype(np.float32)
+	return samples
 
-f = 1.0
-samples2 = ( np.sin(2*np.pi*np.arange(fs*duration)*f/fs) ).astype(np.float32)
+def genWaveEnvelope(fadeIn,sustain,fadeOut):
+	fadeIn = np.arange(float(fadeIn) * fs) / fs 
+	sustain = np.ones(sustain* fs)
+	fadeOut = np.arange(float(fadeOut) * fs,0,-1) / fs 
+	p1 = np.append(fadeIn,sustain)
+	p2 = np.append(p1,fadeOut)
+	return p2
 
-print len(samples)
-samples2 = np.arange(duration * fs) / fs 
+def addToTotal(waveData,startPoint):
+	global totalRes
+	emptyBefore = np.zeros(startPoint * fs)
+	emptyAfter = np.zeros(((totalDuration - startPoint) * fs) - len(waveData))
+	addLayer = np.append(emptyBefore,waveData)
+	addLayer = np.append(addLayer,emptyAfter)
+	totalRes = np.add(totalRes,addLayer)
 
-samples2 = samples2 * 1
+def genWave(freq,fadeIn,sustain,fadeOut):
+	return np.multiply(genSinWave(fadeIn+sustain+fadeOut,freq),genWaveEnvelope(fadeIn,sustain,fadeOut))
+
+for i in range(100):
+	addToTotal(genWave(freqF,15,10,2),5)
 
 
-print len(samples2)
 
-print samples
-print samples2
 
-res = np.multiply(samples2,samples)
-
-plt.plot(res)
-plt.show()
-
-scaled = np.int16(res/np.max(np.abs(res)) * 32767)
+scaled = np.int16(totalRes/np.max(np.abs(totalRes)) * 32767)
 
 write('test.wav', 44100, scaled)
